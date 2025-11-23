400cab2cc307fce2577e6d09cb7271d6aa5a2f44
azkich <100170006+azkich@users.noreply.github.com>
2025-11-23 11:31:18 +0300

    Update ConfigHolder.java

--- src/main/java/betterblockentities/gui/ConfigHolder.java
+++ src/main/java/betterblockentities/gui/ConfigHolder.java
@@ -10,7 +10,14 @@ public class ConfigHolder
     public boolean optimize_beds = true;
     public boolean optimize_bells = true;
     public boolean optimize_decoratedpots = true;
-
+    public boolean optimize_item_frames = true;
+    public boolean optimize_banners = true;
+    public boolean optimize_campfires = true;
+    public boolean optimize_furnaces = true;
+    public boolean optimize_hoppers = true;
+    public boolean optimize_sculk_sensors = true;
+    public boolean optimize_comparators = true;
+    
     public boolean chest_animations = true;
     public boolean render_sign_text = true;
     public boolean shulker_animations = true;
@@ -30,6 +37,13 @@ public class ConfigHolder
         copy.optimize_beds = optimize_beds;
         copy.optimize_bells = optimize_bells;
         copy.optimize_decoratedpots = optimize_decoratedpots;
+        copy.optimize_item_frames = optimize_item_frames;
+        copy.optimize_banners = optimize_banners;
+        copy.optimize_campfires = optimize_campfires;
+        copy.optimize_furnaces = optimize_furnaces;
+        copy.optimize_hoppers = optimize_hoppers;
+        copy.optimize_sculk_sensors = optimize_sculk_sensors;
+        copy.optimize_comparators = optimize_comparators;
         copy.chest_animations = chest_animations;
         copy.render_sign_text = render_sign_text;
         copy.shulker_animations = shulker_animations;
@@ -52,6 +66,13 @@ public class ConfigHolder
                 && optimize_beds == other.optimize_beds
                 && optimize_bells == other.optimize_bells
                 && optimize_decoratedpots == other.optimize_decoratedpots
+                && optimize_item_frames == other.optimize_item_frames
+                && optimize_banners == other.optimize_banners
+                && optimize_campfires == other.optimize_campfires
+                && optimize_furnaces == other.optimize_furnaces
+                && optimize_hoppers == other.optimize_hoppers
+                && optimize_sculk_sensors == other.optimize_sculk_sensors
+                && optimize_comparators == other.optimize_comparators
                 && chest_animations == other.chest_animations
                 && render_sign_text == other.render_sign_text
                 && shulker_animations == other.shulker_animations
